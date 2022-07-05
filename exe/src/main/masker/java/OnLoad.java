560099ddb132981c52d411e5cf53243a2cf95deb
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-07-05 14:42:43 +0700

    Create OnLoad.java

---
+++ exe/src/main/masker/java/OnLoad.java
@@ -0,0 +1,18 @@
+/* OnLoad Game*/
+import javagame.*
+import server.*
+import textinput.*
+
+// Create a fetch loading
+public void initFile[0].studio
+if(load!=loadError)
+tryError()
+studio.LoadingError('Not server and multiple notifying us, Exiting %s' + % (time.timeout))
+if(load==Loaded)
+fetch('https://tuyancong.github.io/MaskerGame/src/level.json').join([0])
+metaMessage('Welcome/Bienvenido!')
+loginLoading()
+sleep(6)
+login('Please login/signup to play a måsker/Você login/signup y play de måsker')
+text.area(login)
+Download('https://tuyancong.github.io/MaskerGame/' + login + '/level')
