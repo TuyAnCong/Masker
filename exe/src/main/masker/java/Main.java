ac7b39320d7aa76aa159d201afd2acf5402f363b
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-07-05 19:14:15 +0700

    Create Main.java

---
+++ exe/src/main/masker/java/Main.java
@@ -0,0 +1,15 @@
+import javagame.*;
+import ytdl.*;
+import rhythm.*;
+public class MainMenu {
+  title.map[title].string("Main Menu")
+    playButton = rhythmAdd("Play", button)
+    hp = rhythmAddHP(hpMax, hp)
+    addButtomCard.href("card.ppf")
+    addButtonCard.create.href("card.ppf", playButton)
+ 
+    card = importLib("ytdl", "java:card", "ppf")
+    c = card.ppf.Create(card.ytdl.thumbnail(RANDOM), card.ytdl.randomName(thumbnail))
+    card.java:card.download(c)
+                     
+}
